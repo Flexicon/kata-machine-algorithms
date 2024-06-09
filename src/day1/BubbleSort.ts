db5d066f8c8ffd4a2ae1f4a9ee656efbdf803c65
Mike Repec <mikerepec@gmail.com>
2024-06-09 10:54:39 +0200

    feat: BubbleSort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length - i; j++) {
+      const a = arr[j - 1];
+      const b = arr[j];
 
+      if (a > b) {
+        arr[j - 1] = b;
+        arr[j] = a;
+      }
+    }
+  }
 }
