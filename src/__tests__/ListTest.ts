a90f8853bcc33bbc480a728b9893081f0c47c62d
Mike Repec <mikerepec@gmail.com>
2024-06-09 16:54:58 +0200

    feat: SinglyLinkedList

--- src/__tests__/ListTest.ts
+++ src/__tests__/ListTest.ts
@@ -9,9 +9,10 @@ export function test_list(list: List<number>): void {
 
     list.append(11);
     expect(list.removeAt(1)).toEqual(9);
+    expect(list.length).toEqual(2);
     expect(list.remove(9)).toEqual(undefined);
+    expect(list.remove(11)).toEqual(11);
     expect(list.removeAt(0)).toEqual(5);
-    expect(list.removeAt(0)).toEqual(11);
     expect(list.length).toEqual(0);
 
     list.prepend(5);
@@ -23,4 +24,32 @@ export function test_list(list: List<number>): void {
     expect(list.remove(9)).toEqual(9);
     expect(list.length).toEqual(2);
     expect(list.get(0)).toEqual(7);
+
+    list.insertAt(1, 0);
+    list.insertAt(2, 0);
+
+    expect(list.get(0)).toEqual(2);
+    expect(list.get(1)).toEqual(1);
+    expect(list.get(2)).toEqual(7);
+    expect(list.get(3)).toEqual(5);
+
+    list.insertAt(9, 2);
+
+    expect(list.get(0)).toEqual(2);
+    expect(list.get(1)).toEqual(1);
+    expect(list.get(2)).toEqual(9);
+    expect(list.get(3)).toEqual(7);
+    expect(list.get(4)).toEqual(5);
+
+    list.insertAt(42, 999);
+
+    expect(list.get(5)).toEqual(42);
+    expect(list.remove(9)).toEqual(9);
+    expect(list.remove(42)).toEqual(42);
+
+    expect(list.get(0)).toEqual(2);
+    expect(list.get(1)).toEqual(1);
+    expect(list.get(2)).toEqual(7);
+    expect(list.get(3)).toEqual(5);
+    expect(list.length).toEqual(4);
 }
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,106 @@
+class Node<T> {
+    constructor(public value: T, public next: Node<T> | undefined = undefined) {
+    }
+}
+
 export default class SinglyLinkedList<T> {
     public length: number;
-
-    
+    private head: Node<T> | undefined;
 
     constructor() {
+        this.length = 0;
     }
 
     prepend(item: T): void {
+        this.head = new Node(item, this.head);
+        this.length++;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (!this.head || idx === 0) {
+            return this.prepend(item);
+        }
 
-}
+        let prevNode = this.head;
+        while (prevNode.next && idx > 1) {
+            prevNode = prevNode.next;
+            idx--;
+        }
+
+        prevNode.next = new Node(item, prevNode.next);
+        this.length++;
+    }
+
     append(item: T): void {
+        if (!this.head) {
+            return this.prepend(item);
+        }
 
-}
+        let node = this.head;
+        while (node.next) {
+            node = node.next;
+        }
+
+        node.next = new Node(item);
+        this.length++;
+    }
+
     remove(item: T): T | undefined {
+        if (!this.head) return undefined;
+        if (this.head.value === item) return this.removeAt(0);
+        if (!this.head.next) return undefined;
 
-}
+        let prevNode = this.head;
+        let node = this.head.next;
+        while (node && node.value !== item && node.next) {
+            prevNode = node;
+            node = node.next;
+        }
+        if (node.value !== item) return undefined;
+
+        prevNode.next = node.next;
+        node.next = undefined;
+        this.length--;
+
+        return node.value;
+    }
+
     get(idx: number): T | undefined {
+        if (!this.head) return undefined;
 
-}
+        let node = this.head;
+        while (node.next && idx > 0) {
+            node = node.next;
+            idx--;
+        }
+        if (idx !== 0) return undefined;
+
+        return node.value;
+    }
+
     removeAt(idx: number): T | undefined {
+        if (!this.head) return undefined;
+        if (idx === 0) {
+            const node = this.head;
+            this.head = node.next;
+            node.next = undefined;
+            this.length--;
 
-}
+            return node.value;
+        }
+
+        let prevNode = this.head;
+        while (prevNode.next && idx > 1) {
+            prevNode = prevNode.next;
+            idx--;
+        }
+        if (idx !== 1) return undefined;
+
+        const node = prevNode.next;
+        prevNode.next = node?.next;
+        if (node) node.next = undefined;
+        this.length--;
+
+        return node?.value;
+    }
 }
