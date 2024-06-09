bbcd6b3fae248d1a44abcb098c51823a1fee8835
Mike Repec <mikerepec@gmail.com>
2024-06-09 10:07:48 +0200

    feat: BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lft = 0, rgt = haystack.length;
 
+  do {
+    const cur = Math.floor(lft + (rgt - lft) / 2);
+    const val = haystack[cur];
+
+    if (val === needle) return true;
+    if (val > needle) rgt = cur;
+    if (val < needle) lft = cur + 1;
+  } while (lft < rgt)
+
+  return false;
 }
