6b3b1fb7180f67f575001c33b63523765fca3008
Mike Repec <mikerepec@gmail.com>
2024-06-09 10:30:00 +0200

    feat: TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const step = Math.floor(Math.sqrt(breaks.length));
 
+  // Find last unbroken point via step hops
+  let i = 0;
+  for (; i < breaks.length; i += step) {
+    if (breaks[i]) break;
+  }
+  i -= step; // Rewind to last known unbroken point
+
+  // Find first breaking point by scanning from last known unbroken point
+  for (let j = 0; j < step && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) return i;
+  }
+
+  return -1;
 }
